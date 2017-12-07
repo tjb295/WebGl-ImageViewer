c9b580fe330538cca874028599b775f810f2c864
Thomas Back <tjb295@nau.edu>
2017-12-07 15:56:27 -0700

    Linking program not successful after adding the matrix multiplier in

--- demo.js
+++ demo.js
@@ -1,12 +1,15 @@
 var vertexShaderSource = `#version 300 es
 
 in vec4 a_position;
-in vec2 a_texcoord;
+inout mat3 u_matrix;
 
 out vec2 v_texcoord;
 
 void main() {
+
+  //multiply position by matrix
   gl_Position = a_position;
+  vec2 position = (u_matrix * vec3(a_position, 1)).xy;
   v_texcoord = a_texcoord;
 }
 `;
@@ -62,6 +65,7 @@ function main() {
   var positionLocation = gl.getAttribLocation(program, "a_position");
   var texcoordLocation = gl.getAttribLocation(program, "a_texcoord");
   var textureLocation = gl.getUniformLocation(program, "u_texture");
+  var matrixLocation = gl.getUniformLocation(program, "u_matrix");
 
   var vao = gl.createVertexArray();
 
@@ -135,6 +139,17 @@ function main() {
     gl.activeTexture(gl.TEXTURE0);
     gl.bindTexture(gl.TEXTURE_2D, image);
 
+    var translationMatrix = matrix_multiply.translation(document.getElementById("x").value, document.getElementById("y").value);
+    var rotationMatrix = matrix_multiply.rotation(document.getElementById("angle").value);
+    var scaleMatrix = matrix_multiply.scaling(document.getElementById("x").value, document.getElementById("y").value);
+
+    //multiply matrices
+    var matrix = matrix_multiply.multiply(translationMatrix, rotationMatrix);
+    matrix = matrix_multiply.multiply(matrix, scaleMatrix);
+
+    //set matrix
+    gl.uniformMatrix3fv(matrixLocation, false, matrix);
+
     gl.drawArrays(gl.TRIANGLES, 0, 6);
 
   }
@@ -172,5 +187,37 @@ var matrix_multiply = {
       0, sy, 0,
       0, 0 ,1,
     ]
-  }
+  },
+  multiply: function(a,b){
+    var a00 = a[0 * 3 + 0];
+    var a01 = a[0 * 3 + 1];
+    var a02 = a[0 * 3 + 2];
+    var a10 = a[1 * 3 + 0];
+    var a11 = a[1 * 3 + 1];
+    var a12 = a[1 * 3 + 2];
+    var a20 = a[2 * 3 + 0];
+    var a21 = a[2 * 3 + 1];
+    var a22 = a[2 * 3 + 2];
+    var b00 = b[0 * 3 + 0];
+    var b01 = b[0 * 3 + 1];
+    var b02 = b[0 * 3 + 2];
+    var b10 = b[1 * 3 + 0];
+    var b11 = b[1 * 3 + 1];
+    var b12 = b[1 * 3 + 2];
+    var b20 = b[2 * 3 + 0];
+    var b21 = b[2 * 3 + 1];
+    var b22 = b[2 * 3 + 2];
+    return [
+      b00 * a00 + b01 * a10 + b02 * a20,
+      b00 * a01 + b01 * a11 + b02 * a21,
+      b00 * a02 + b01 * a12 + b02 * a22,
+      b10 * a00 + b11 * a10 + b12 * a20,
+      b10 * a01 + b11 * a11 + b12 * a21,
+      b10 * a02 + b11 * a12 + b12 * a22,
+      b20 * a00 + b21 * a10 + b22 * a20,
+      b20 * a01 + b21 * a11 + b22 * a21,
+      b20 * a02 + b21 * a12 + b22 * a22,
+    ];
+  },
+  
 }
